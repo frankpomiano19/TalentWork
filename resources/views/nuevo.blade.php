3fda191b1de58dbdcf61d3d7cbf6ed606344839b
HuamanAngel <oxipusioxx12@gmail.com>
2021-06-16 21:22:01 -0500

    Proyecto Base

---
+++ resources/views/nuevo.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="bg-white display-1">GATO CON BOTASS</div>
+ACA
+<i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i>
+
+
+{{-- <script>
+
+Swal.fire('Any fool can use a computer');
+</script> --}}
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+
+@section('contenido_js')
+    
+@endsection
+
+@section('contenido_cSS')
+    
+@endsection
+
+
+@section('content')
+<br>
+<br>
+<br>
+<br>
+dasds
+<a href=""></a>
+<i class="fa fa-home" aria-hidden="true"></i>
+<div class="animate__animated animate__rubberBand"> cascasd</div>
+@endsection
+
+@section('contenido_abajo_js')
+<script>
+
+Swal.fire('Any fool can use a computer');
+</script>
+    
+@endsection
