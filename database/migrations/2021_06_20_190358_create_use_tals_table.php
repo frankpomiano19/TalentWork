6d86b7d9f7f0cb91c2a7b43fa4107ada05fc35ba
HuamanAngel <oxipusioxx12@gmail.com>
2021-06-20 15:21:11 -0500

    Base de datos

---
+++ database/migrations/2021_06_20_190358_create_use_tals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUseTalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('use_tals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('use_id');
+            $table->unsignedBigInteger('ser_tal_id');
+            $table->foreign('use_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('ser_tal_id')->references('id')->on('service_talent')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('use_tals');
+    }
+}
