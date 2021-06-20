12b8f91d93ff75b272eedc099e1a9ea8b5099e1a
Andres Maita <andres.maita@unmsm.edu.pe>
2021-06-20 15:47:21 -0500

    error base de datos corregido

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
-class User extends Eloquent
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
