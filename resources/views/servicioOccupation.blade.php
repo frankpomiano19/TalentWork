9cc5953b5eed4aaf6b45df5fb5480f264f8444a4
Lum73 <alexandra.gonzales1@unmsm.edu.pe>
2021-07-08 03:19:06 -0500

    chat

--- resources/views/servicioOccupation.blade.php
+++ resources/views/servicioOccupation.blade.php
@@ -88,7 +88,12 @@
                                             Ya lo contrataste
                                         </button>
                                         <br>
-                                        <div class="text-danger">* Para comunicarte con el que ofrece el servicio, presione <a href="">AQUI</a> </div>                                
+                                        <div class="text-danger">* Para comunicarte con el que ofrece el servicio, presione chat: </div>   
+                               
+                                       
+                                     
+                                       
+                                                        
                                     @else
                                         <button class="btn btn-outline-dark flex-shrink-0 btn-details-now-data" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                             <i class="bi-cart-fill me-1"></i>
@@ -175,6 +180,136 @@
 
 
 
+
+
+
+
+
+
+
+
+  <button class="open-button" onclick="openForm()">
+     <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Chat</font>
+     </font>
+  </button>
+        
+    <div class="chat-popup" id="myForm" style="display: none;">
+        <form action="/action_page.php" class="form-container">
+
+           
+            <div class="portlet portlet-default">
+                <div class="portlet-heading">
+
+                    <div class="portlet-title">
+                        <h4><i class="fa fa-circle text-green"></i> Jane Smith</h4>
+                    </div>
+
+                    <div class="portlet-widgets">
+                        <span class="divider"></span>
+                        <a data-toggle="collapse" data-parent="#accordion" href="#chat"><i class="fa fa-chevron-down"></i></a>
+                    </div>
+
+                    <div class="clearfix"></div>
+                </div>
+                
+
+                  
+                    <div class="portlet-body chat-widget" style="overflow-y: auto; width: auto; height: 300px;">
+
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p class="text-center text-muted small">January 1, 2014 at 12:23 PM</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="media">
+                                    <a class="pull-left" href="#">
+                                        <img class="media-object img-circle img-chat" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+                                    </a>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Jane Smith
+                                            <span class="small pull-right">12:23 PM</span>
+                                        </h4>
+                                        <p>Hi, I wanted to make sure you got the latest product report. Did Roddy get it to you?</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                
+
+                        <hr>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="media">
+                                    <a class="pull-left" href="#">
+                                        <img class="media-object img-circle img-chat" src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="">
+                                    </a>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">John Smith
+                                            <span class="small pull-right">12:28 PM</span>
+                                        </h4>
+                                        <p>Yeah I did. Everything looks good.</p>
+                                        <p>Did you have an update on purchase order #302?</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr>
+                        
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="media">
+                                    <a class="pull-left" href="#">
+                                        <img class="media-object img-circle img-chat" src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="">
+                                    </a>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Jane Smith
+                                            <span class="small pull-right">12:39 PM</span>
+                                        </h4>
+                                        <p>No not yet, the transaction hasn't cleared yet. I will let you know as soon as everything goes through. Any idea where you want to get lunch today?</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr>
+                    
+                    </div>     
+               
+            </div>
+        
+        
+              <div class="portlet-footer">
+                            <div class="form-group">                        
+                            <input class="form-control form-control-lg" type="text" placeholder="Enter message...">
+                            </div>
+                            <div class="form-group">
+                                <button type="button" class="btn btn-default pull-right">Send</button>
+                                <div class="clearfix"></div>
+                            </div>
+                       
+
+                </div>     
+
+        
+        
+
+        <button type="button" class="btn cancel" onclick="closeForm()"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Close</font></font></button>
+        </form>
+    </div>
+
+
+
+
+
+
+
+
 @endsection
 
 @section('contenido_abajo_js')
@@ -206,4 +341,22 @@
     });
 </script>
 @endif
+
+
+<script>
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+</script>
+
 @endsection
+
+
+
+
+
+
