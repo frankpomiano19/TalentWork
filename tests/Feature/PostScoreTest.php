a7fd7eb830504604e0def702afb08e3d9b647be0
Vladimir <frank_a.pardo_2017@hotmail.com>
2021-08-08 02:23:45 -0500

    cus05 terminado

---
+++ tests/Feature/PostScoreTest.php
@@ -0,0 +1,277 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Route;
+use App\Models\ServiceOccupation;
+use App\Models\ServiceTalent;
+use App\Models\use_occ;
+use App\Models\use_tal;
+use App\Models\Post_comment;
+use App\Models\Question;
+use App\Models\Score;
+use App\Models\Contract;
+use Illuminate\Http\Request;
+use Illuminate\Http\Controllers\PerfilController;
+use Illuminate\Http\Controllers\ServiceController;
+use Illuminate\Http\Controllers\PostScoreController;
+use Illuminate\Http\Controllers\ContractController;
+
+class PostScoreTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    public function test_profileSO_view()
+    {
+        $id = '2';
+        $allServices = use_occ::where('id',$id)->first();
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $view = $this->get(route('showProfileServiceOccupation',$allServices->id))->assertStatus(200);
+        $view->assertSee('Califica este servicio');
+    }
+
+    public function test_profileST_view()
+    {
+        $id = '1';
+        $allServices = use_tal::where('id',$id)->first();
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $view = $this->get(route('showProfileServiceTalent',$allServices->id))->assertStatus(200);
+        $view->assertSee('Califica este servicio');
+    }
+    
+    public function test_postScoreNull_OCC()
+    {
+        //$this->withoutExceptionHandling();
+        $calificacion = null;
+        $value= '1';
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $id = '2';
+        $allServices = use_occ::where('id',$id)->first();
+
+        $view = $this->get(route('showProfileServiceOccupation',$allServices->id))->assertStatus(200);
+
+        $response = $this->from('/profileServiceOccupation/2')->post(route('registrarScore'), [ 
+                                                                    'calificacion' => $calificacion,
+                                                                    'usCom' => auth()->user()->id,
+                                                                    'typeJobFromScore' => $value,
+                                                                    'serviceId' => $allServices->id,
+                                                                    'etiqueta'=> 'comentado',]
+                                )->assertRedirect('/profileServiceOccupation/2');
+
+        // $response = $this->assertDatabaseMissing('Post_comments', [
+        //                             'comentario' => $comentario
+        //                         ]);
+        
+    }
+
+    public function test_postScoreNull_TAL()
+    {
+        //$this->withoutExceptionHandling();
+        $calificacion = null;
+        $value= '2';
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $id = '1';
+        $allServices = use_tal::where('id',$id)->first();
+
+        $view = $this->get(route('showProfileServiceTalent',$allServices->id))->assertStatus(200);
+
+        $response = $this->from('/profileServiceTalent/1')->post(route('registrarScore'), [ 
+                                                                    'calificacion' => $calificacion,
+                                                                    'usCom' => auth()->user()->id,
+                                                                    'typeJobFromScore' => $value,
+                                                                    'serviceId' => $allServices->id,
+                                                                    'etiqueta'=> 'comentado',]
+                                )->assertRedirect('/profileServiceTalent/1');
+
+        // $response = $this->assertDatabaseMissing('Post_comments', [
+        //                             'comentario' => $comentario
+        //                         ]);
+        
+    }
+
+    public function test_postScoreData_OCC()
+    {
+    
+        $calificacion = 4;
+        $value= '1';
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $id = '2';
+        $allServices = use_occ::where('id',$id)->first();
+
+        $view = $this->get(route('showProfileServiceOccupation',$allServices->id))->assertStatus(200);
+
+        $response = $this->from('/profileServiceOccupation/2')->post(route('registrarScore'), [ 
+                                                                    'calificacion' => $calificacion,
+                                                                    'usCom' => auth()->user()->id,
+                                                                    'typeJobFromScore' => $value,
+                                                                    'serviceId' => $allServices->id,
+                                                                    'etiqueta'=> 'comentado',]
+                                )->assertRedirect('/profileServiceOccupation/2');
+        
+    }
+
+    public function test_postScoreData_TAL()
+    {
+        //$this->withoutExceptionHandling();
+        $calificacion = 3;
+        $value= '2';
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $id = '1';
+        $allServices = use_tal::where('id',$id)->first();
+
+        $view = $this->get(route('showProfileServiceTalent',$allServices->id))->assertStatus(200);
+
+        $response = $this->from('/profileServiceTalent/1')->post(route('registrarScore'), [ 
+                                                                    'calificacion' => $calificacion,
+                                                                    'usCom' => auth()->user()->id,
+                                                                    'typeJobFromScore' => $value,
+                                                                    'serviceId' => $allServices->id,
+                                                                    'etiqueta'=> 'comentado',]
+                                )->assertRedirect('/profileServiceTalent/1');
+        
+    }
+
+    public function test_postScoreDefault_OCC()
+    {
+        //$this->withoutExceptionHandling();
+        $calificacion = 5;
+        $value= '';
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $id = '2';
+        $allServices = use_occ::where('id',$id)->first();
+
+        $view = $this->get(route('showProfileServiceOccupation',$allServices->id))->assertStatus(200);
+
+        $response = $this->from('/profileServiceOccupation/2')->post(route('registrarScore'), [ 
+                                                                    'calificacion' => $calificacion,
+                                                                    'usCom' => auth()->user()->id,
+                                                                    'typeJobFromScore' => $value,
+                                                                    'serviceId' => $allServices->id,
+                                                                    'etiqueta'=> 'comentado',]
+                                );
+        
+        $response = $this->assertDatabaseMissing('Scores', [
+                                    'calificacion' => $calificacion
+                                ]);
+        
+    }
+
+    public function test_postScoreDefault_TAL()
+    {
+        //$this->withoutExceptionHandling();
+        $calificacion = 5;
+        $value= '';
+
+        $credentials = [
+            "email" => "alvarado4@unmsm.edu.pe",
+            "password" => "perrovaca",
+        ];
+        $this->post('login', $credentials);
+
+        $id = '1';
+        $allServices = use_tal::where('id',$id)->first();
+
+        $view = $this->get(route('showProfileServiceTalent',$allServices->id))->assertStatus(200);
+
+        $response = $this->from('/profileServiceTalent/1')->post(route('registrarScore'), [ 
+                                                                    'calificacion' => $calificacion,
+                                                                    'usCom' => auth()->user()->id,
+                                                                    'typeJobFromScore' => $value,
+                                                                    'serviceId' => $allServices->id,
+                                                                    'etiqueta'=> 'comentado',]
+                                );
+
+        $response = $this->assertDatabaseMissing('Scores', [
+                                    'calificacion' => $calificacion
+                                ]);
+
+    }
+
+    public function test_baseCreatePostScoreOCC_validation()
+    {
+        $calificacion = 0;
+        $use_id = '7';
+        $use_occ_id ='2';
+        $etiqueta = 'comentado';
+
+        $response = $this->assertDatabaseHas('Scores', [
+                    'calificacion'=>$calificacion, 
+                    'use_id'=>$use_id, 
+                    'use_occ_id'=>$use_occ_id,
+                    'etiqueta'=>$etiqueta 
+    
+        ]);
+    }
+
+    public function test_baseCreatePostScoreTAL_validation()
+    {
+        $calificacion = 3;
+        $use_id = '7';
+        $use_tal_id ='1';
+        $etiqueta = 'comentado';
+
+        $response = $this->assertDatabaseHas('Scores', [
+                    'calificacion'=>$calificacion, 
+                    'use_id'=>$use_id, 
+                    'use_tal_id'=>$use_tal_id,
+                    'etiqueta'=>$etiqueta 
+    
+        ]);
+    }
+
+}
